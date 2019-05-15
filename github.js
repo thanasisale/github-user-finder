2a02baae1b46d308279f3f87462a4683929c33fb
Thanasis <28050747+thanasisale@users.noreply.github.com>
2019-05-15 15:39:32 +0300

    github file

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'id';
+        this.client_secret = 'secret';
+        this.repos_count = 5;
+        this.repos_short = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&short=${this.repos_short}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
